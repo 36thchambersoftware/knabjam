a78efcb19c4ee9ae62f6b5ae1662c2ab5dfd1339
Aaron Priebe <preeb@proton.me>
2024-03-29 18:24:52 -0500

    Reording per lint

--- pkg/knabjam/user.go
+++ pkg/knabjam/user.go
@@ -1,14 +1,14 @@
 package knabjam
 
 type User struct {
-	id        uint
 	Name      string
 	Address   string // Cardano wallet address like addr1qy2hmlj...
-	Level     uint
-	Cards     []Card
 	Bio       string
-	Pic       Card // Allow selecting from cards owned.
-	Pro       bool // User subscribes/pays
-	Following []User
 	Role      string // TODO: Should be a slice of Roles: Collector|Creator
+	Cards     []Card
+	Following []User
+	Pic       Card // Allow selecting from cards owned.
+	id        uint
+	Level     uint
+	Pro       bool // User subscribes/pays
 }
